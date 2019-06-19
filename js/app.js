d91805c53e2c080bbba662a1d7f690272e62d528
hemanth <gautham.hemanth@gmail.com>
2019-06-18 21:14:27 -0400

    card flip, card comparision, matching pair style change

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const cards = ['cloud','cloud','traffic','traffic','favorite','favorite','computer','computer',
     'android','android','business','business','flag','flag','wallpaper','wallpaper'];
-console.log(cards);
+
+let arraylist = ['box','hide'];
+let openCardList =[];
 
 function Createcards(array){
 
@@ -8,11 +10,9 @@ function Createcards(array){
     console.log(Wrapper);
     for (let i=0; i< array.length; i++){
         const box = document.createElement('li');
-        box.classList.add('box');
-        box.classList.add('hide');
+        box.classList.add(...arraylist);
         box.innerHTML = `<p><i class="material-icons">${array[i]}</i></p>`;
         Wrapper.appendChild(box);
-
     }
 }
  function shuffle(array) {
@@ -27,5 +27,49 @@ return array;
 }
 
 let shuffleCards = shuffle(cards);
+console.log(shuffleCards);
 Createcards(shuffleCards);
+
+function closeCard(){
+    let cards = document.querySelectorAll('li');
+}
+
+let lastPickedCard =null;
+let currentPickedCard;
+
+let parentElement = document.querySelector('ul');
+let openCounter =0;
+parentElement.addEventListener('click', function respondToClick(childElement){
+    console.log('Item clicked'+ childElement.target.innerHTML);
+    if (childElement.target.nodeName === 'LI' && lastPickedCard !== childElement.target){
+        childElement.target.classList.toggle('hide');
+            if(lastPickedCard == null){
+                lastPickedCard = childElement.target;
+                console.log(lastPickedCard, childElement.target.textContent);
+
+            } else if (compareCards(lastPickedCard.textContent, childElement.target.textContent)){
+                console.log ('Match found');
+                lastPickedCard.classList.add('match');
+                childElement.target.classList.add('match');
+                openCardList.push(lastPickedCard.textContent, childElement.target.textContent);
+                lastPickedCard =null;
+                }
+                else if (!compareCards(lastPickedCard.textContent, childElement.target.textContent)){
+                setTimeout(function toggleCards() {
+                    lastPickedCard.classList.toggle('hide');
+                    childElement.target.classList.toggle('hide');
+                    lastPickedCard =null;
+                },2000);
+            }
+    }
+});
+
+function compareCards(cardA, cardB){
+    if (cardA == cardB){
+        return true;
+    }
+    else return false;
+}
+
+
 
